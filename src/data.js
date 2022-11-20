0b4a4e6802402428ba815bb99e56422e550defaa
Ayako-Yokoe <niluvsuall@gmail.com>
2022-11-20 14:30:23 -0800

    Add stack

--- src/data.js
+++ src/data.js
@@ -5,6 +5,7 @@ export const projects = [
     title: "E-Commerce App: Kids-Clothes",
     caption:
       "E-Commerce Kids-Clothes is a MERN stack application, where admins can add, edit, and delete products based on CRUD operation, and clients can browse and purchase the products utilizing Stripe.",
+    tech: "React, Redux Toolkit, Axios, Node.js, Express, MongoDB, Firebase, Styled-Components, MUI, Stripe",
     gitHubLink: "https://github.com/Ayako-Yokoe/E-Commerce_Kids-Clothes",
     deployedLink: "https://ecommerce-kids-clothes.herokuapp.com/login",
   },
@@ -14,6 +15,7 @@ export const projects = [
     title: "API Project: Collocation Dictionary",
     caption:
       "Collocation-Dictionary is a react web application utilizing fetching API to search collocations and their usage, along with practice and quiz features for personal learning.",
+    tech: "React, Redux Toolkit, Axios, Node.js, Express, MongoDB, Styled-Components, MUI, React-quizlet-flashcard",
     gitHubLink: "https://github.com/Ayako-Yokoe/Collocation-Dictionary",
     deployedLink: "https://collocation-dictionary.herokuapp.com/",
   },
@@ -22,7 +24,8 @@ export const projects = [
     image: "assets/udemy.png",
     title: "Clone Project: Clone-Udemy",
     caption:
-      "Clone-Udemy is a clone project that creates a Udemy landing page with React, worked with other group members utilizing GitHub, Slack, and Trello. We learned and handled marge conflicts and issues.",
+      "Clone-Udemy is a clone project that creates a Udemy landing page with React, worked with other group members utilizing GitHub, Slack, and Trello. We learned and handled merge conflicts and issues.",
+    tech: "React, Styled-Components, MUI, react-items-carousel",
     gitHubLink: "https://github.com/Ayako-Yokoe/clone-udemy/tree/main",
     deployedLink: "https://clone-udemy-landing-page.netlify.app/",
   },
